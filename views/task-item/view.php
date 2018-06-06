8cb29adf65e47e0153db55e2b796a24db0120840
gang.dun <dungang@126.com>
2018-06-06 16:17:24 +0800

    fix task-item-view plan

--- views/task-item/view.php
+++ views/task-item/view.php
@@ -2,13 +2,17 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\models\TaskPlan;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\TaskItem */
+$planName = '无';
+$plan = TaskPlan::findOne(['id'=>$model->plan_id]);
 
 $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => '任务计划', 'url' => ['/task-item','TaskItemSearch[plan_id]'=>$model->plan_id]];
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="modal-header">
 	<button type="button" class="close" data-dismiss="modal"
@@ -20,7 +24,10 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             'id',
-            'plan_id',
+            [
+                'attribute'=>'plan_id',
+                'value'=>$plan == null ? $planName: $plan->name,
+            ],
             'status_code',
             'code',
             'name',
