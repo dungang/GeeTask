a766cd7aba24ecf701a12e6e828f4bf5c5a0993a
顿刚 <dungang@126.com>
2018-06-11 21:40:29 +0800

    Update params.php
    remove key

--- config/params.php
+++ config/params.php
@@ -4,8 +4,8 @@ return [
     'adminEmail' => 'admin@loglass.com',
     'aliyunLog'=> [
         'endpoint'=>'cn-hangzhou.log.aliyuncs.com',
-        'accessKey'=>'LTAI1alt8j7A1BNl',
-        'secretKey'=>'4ErL4LRTaD8Kj1CFKtRNqWRVXyfgfE',
+        'accessKey'=>'',
+        'secretKey'=>'',
     ],
     'baidutongji'=>[
         'loginUrl'=>'',
